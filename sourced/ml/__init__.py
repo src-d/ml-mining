c1a43c86bb65ff5c9b12452ad29590b553a0e26d
Romain Keramitas <r.keramitas@gmail.com>
2019-09-27 16:58:21 +0200

    Update sourced/ml/__init__.py for new structure
    Signed-off-by: Romain Keramitas <r.keramitas@gmail.com>

--- sourced/ml/__init__.py
+++ sourced/ml/__init__.py
@@ -1,1 +1,5 @@
 """MLonCode data mining playground."""
+# You must not include any other code and data in a namespace package's __init__.py
+import pkg_resources
+
+pkg_resources.declare_namespace(__name__)
