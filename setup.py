ef8f22983e4eb16554aa36516db0e20493f07daa
Romain Keramitas <r.keramitas@gmail.com>
2019-09-27 16:57:55 +0200

    Update setup.py
    Signed-off-by: Romain Keramitas <r.keramitas@gmail.com>

--- setup.py
+++ setup.py
@@ -4,13 +4,15 @@ import os.path
 
 from setuptools import find_packages, setup
 
-sourcedml = SourceFileLoader("sourced-ml-mining", "./sourced/ml/mining/__init__.py").load_module()
+sourcedml = SourceFileLoader(
+    "sourced-ml-mining", "./sourced/ml/mining/__init__.py"
+).load_module()
 
-with io.open(os.path.join(os.path.dirname(__file__), "README.md"), encoding="utf-8") as f:
+with io.open(
+    os.path.join(os.path.dirname(__file__), "README.md"), encoding="utf-8"
+) as f:
     long_description = f.read()
 
-tf_requires = ["tensorflow>=1.0,<2.0"]
-tf_gpu_requires = ["tensorflow-gpu>=1.0,<2.0"]
 exclude_packages = (
     ("sourced.ml.mining.tests", "sourced.ml.mining.tests.source")
     if not os.getenv("ML_MINING_SETUP_INCLUDE_TESTS", False)
@@ -20,8 +22,8 @@ exclude_packages = (
 
 setup(
     name="sourced-ml-mining",
-    description="Framework for mining large repositories to be used in machine learning on source "
-                "code. Provides API and tools to mine source code at scale.",
+    description="Tools for mining Git repositories and training machine learning on source "
+    "code models.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     version=sourcedml.__version__,
@@ -31,34 +33,23 @@ setup(
     url="https://github.com/src-d/ml-mining",
     download_url="https://github.com/src-d/ml-mining",
     packages=find_packages(exclude=exclude_packages),
-    namespace_packages=["sourced"],
-    keywords=[
-        "machine learning on source code",
-        "github",
-        "bblfsh",
-        "babelfish",
-    ],
+    namespace_packages=["sourced", "sourced.ml"],
+    entry_points={"console_scripts": ["srcdmine=sourced.ml.mining.__main__:main"]},
+    keywords=["machine learning on source code", "github", "bblfsh", "babelfish"],
     install_requires=[
-        "sourced-ml-core",
+        "sourced-ml-core>=0.0.5",
         "PyStemmer>=1.3,<2.0",
-        "sourced-jgit-spark-connector>=2.0.1,<2.1.0",
         "humanize>=0.5.0,<0.6",
-        "parquet>=1.2,<2.0",
     ],
-    extras_require={"tf": tf_requires, "tf_gpu": tf_gpu_requires},
     tests_require=["docker>=3.6.0,<4.0"],
-    package_data={
-        "": ["LICENSE.md", "README.md"],
-        "sourced.ml.mining.tests": ["./asdf/*.asdf", "./swivel/*", "identifiers.csv.tar.gz"],
-    },
-    python_requires=">=3.5",
+    package_data={"": ["LICENSE.md", "README.md"]},
+    python_requires=">=3.6",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Environment :: Console",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: POSIX",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Topic :: Software Development :: Libraries",
