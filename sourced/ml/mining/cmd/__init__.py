665f9ea0a8e3921c8f4574115c750990baeb20c8
Romain Keramitas <r.keramitas@gmail.com>
2019-10-16 06:36:16 +0200

    Add stdlib extraction logic
    Signed-off-by: Romain Keramitas <r.keramitas@gmail.com>

--- sourced/ml/mining/cmd/__init__.py
+++ sourced/ml/mining/cmd/__init__.py
@@ -1,3 +1,5 @@
 # flake8: noqa
 from sourced.ml.mining.cmd.args import ArgumentDefaultsHelpFormatterNoNone
 from sourced.ml.mining.cmd.clickhouse2deps import clickhouse2deps, CLICKHOUSE_LANGS
+from sourced.ml.mining.cmd.collect_stdlibs import collect_stdlibs
+
