9a835c8fe4e1c2ba471904611ee264d7655f66cb
Romain Keramitas <r.keramitas@gmail.com>
2019-10-22 17:50:00 +0200

    Add Swivel input creation util
    Signed-off-by: Romain Keramitas <r.keramitas@gmail.com>

--- sourced/ml/mining/utils/__init__.py
+++ sourced/ml/mining/utils/__init__.py
@@ -1,3 +1,9 @@
 # flake8: noqa
-from sourced.ml.mining.utils.fs import check_remove_filepath, path_with_suffix
+from sourced.ml.mining.utils.fs import (
+    check_exists_filepath,
+    check_empty_directory,
+    check_remove_filepath,
+    path_with_suffix,
+)
+from sourced.ml.mining.utils.swivel import create_swivel_inputs
 
